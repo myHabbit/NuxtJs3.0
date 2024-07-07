c45028a3a54a5abe3c0874869518a6ce274e15bd
Mr wei <2857736333@qq.com>
2024-07-07 14:58:08 +0800

    NuxtJs3.0：初始化页面

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+ <!-- Nuxt页面路由  相当于routerView -->
+  <nuxt-link to="/">首页</nuxt-link>
+  <nuxt-link to="/video">视频页</nuxt-link>
+  <NuxtPage></NuxtPage>
 </template>
+
+
---
+++ pages/video/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        视频页
+    </div>
+</template>
+<script lang="" setup>
+
+</script>
+<style>
+    
+</style>
